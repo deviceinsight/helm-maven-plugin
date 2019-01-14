05efab829f23a7a204e5d96edd29b93027b46a20
Paul Vorbach <paul.vorbach@device-insight.com>
2019-01-14 09:21:43 +0100

    Add Apache License

--- src/main/kotlin/com/deviceinsight/helm/TemplateMojo.kt
+++ src/main/kotlin/com/deviceinsight/helm/TemplateMojo.kt
@@ -1,4 +1,20 @@
-package com.deviceinsight.helmdeploymavenplugin
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.deviceinsight.helm
 
 import org.apache.maven.plugin.MojoExecutionException
 import org.apache.maven.plugins.annotations.LifecyclePhase
@@ -6,7 +22,8 @@ import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
 import java.io.File
 
-abstract class AbstractTemplateMojo : AbstractHelmMojo() {
+@Mojo(name = "template", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST)
+class TemplateMojo : AbstractHelmMojo() {
 
 	/**
 	 * An optional values.yaml file that is used to render the template, relative to `${project.basedir}`.
@@ -56,16 +73,3 @@ abstract class AbstractTemplateMojo : AbstractHelmMojo() {
 		}
 	}
 }
-
-/**
- * define mojo for goal "helm-template"
- */
-@Mojo(name = "helm-template", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST)
-class HelmTemplateMojo : AbstractTemplateMojo()
-
-/**
- * define mojo for goal "template"
- */
-@Mojo(name = "template", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST)
-class TemplateMojo : AbstractTemplateMojo()
-
