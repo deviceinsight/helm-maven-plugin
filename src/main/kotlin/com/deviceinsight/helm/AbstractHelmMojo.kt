d25c4cf8eeca80e43c54cfd16adcc25d126b603f
kobynet <kobynet@gmail.com>
2021-02-11 09:24:06 +0100

    Fix parallel binary download (#7)
    * Added resolve goal for cases where the download is made in parallel,
      we can use the resolve goal as separate goal just before running the
      maven build. This change is backward-compatible.
    
    Fixes #7

--- src/main/kotlin/com/deviceinsight/helm/AbstractHelmMojo.kt
+++ src/main/kotlin/com/deviceinsight/helm/AbstractHelmMojo.kt
@@ -16,55 +16,18 @@
 
 package com.deviceinsight.helm
 
-import com.deviceinsight.helm.util.PlatformDetector
-import org.apache.maven.artifact.Artifact
-import org.apache.maven.artifact.repository.ArtifactRepository
-import org.apache.maven.artifact.resolver.ArtifactResolutionRequest
-import org.apache.maven.artifact.resolver.ArtifactResolutionResult
 import org.apache.maven.plugin.AbstractMojo
-import org.apache.maven.plugins.annotations.Component
 import org.apache.maven.plugins.annotations.Parameter
 import org.apache.maven.project.MavenProject
-import org.apache.maven.repository.RepositorySystem
 import java.io.File
-import java.net.HttpURLConnection
-import java.net.URI
-import java.net.URL
-import java.nio.file.Files
-import java.nio.file.Path
-import java.nio.file.StandardCopyOption
-import java.util.zip.ZipEntry
-import java.util.zip.ZipInputStream
-import kotlin.system.measureTimeMillis
 
 abstract class AbstractHelmMojo : AbstractMojo() {
-
-	@Parameter(property = "helmGroupId", defaultValue = "com.deviceinsight.helm")
-	private lateinit var helmGroupId: String
-
-	@Parameter(property = "helmArtifactId", defaultValue = "helm")
-	private lateinit var helmArtifactId: String
-
-	@Parameter(property = "helmVersion", required = true)
-	private lateinit var helmVersion: String
-
-	@Parameter(property = "helmDownloadUrl", defaultValue = "https://get.helm.sh/")
-	private lateinit var helmDownloadUrl: URI
 
 	@Parameter(property = "chartVersion", required = false, defaultValue = "\${project.model.version}")
 	protected lateinit var chartVersion: String
 
 	@Parameter(defaultValue = "\${project}", readonly = true, required = true)
 	protected lateinit var project: MavenProject
-
-	@Parameter(readonly = true, required = true, defaultValue = "\${localRepository}")
-	private lateinit var localRepository: ArtifactRepository
-
-	@Parameter(readonly = true, required = true, defaultValue = "\${project.remoteArtifactRepositories}")
-	private lateinit var remoteRepositories: List<ArtifactRepository>
-
-	@Component
-	private lateinit var repositorySystem: RepositorySystem
 
 	@Parameter(property = "chartFolder", required = false)
 	private var chartFolder: String? = null
@@ -74,31 +37,6 @@ abstract class AbstractHelmMojo : AbstractMojo() {
 	 */
 	@Parameter(property = "chartName", required = false)
 	private var chartName: String? = null
-
-	protected fun resolveHelmBinary(): String {
-
-		val platformIdentifier = PlatformDetector.detectHelmReleasePlatformIdentifier()
-		val helmArtifact: Artifact =
-			repositorySystem.createArtifactWithClassifier(helmGroupId, helmArtifactId, helmVersion, "binary",
-				platformIdentifier)
-
-		val request = ArtifactResolutionRequest()
-		request.artifact = helmArtifact
-		request.isResolveTransitively = false
-		request.localRepository = localRepository
-		request.remoteRepositories = remoteRepositories
-
-		val resolutionResult: ArtifactResolutionResult = repositorySystem.resolve(request)
-
-		if (!resolutionResult.isSuccess) {
-			log.info("Artifact not found in remote repositories")
-			downloadAndInstallHelm(helmArtifact, platformIdentifier)
-		}
-
-		helmArtifact.file.setExecutable(true)
-
-		return helmArtifact.file.absolutePath
-	}
 
 	protected fun executeCmd(cmd: String, directory: File = target(),
 							 redirectOutput: ProcessBuilder.Redirect = ProcessBuilder.Redirect.PIPE) {
@@ -119,7 +57,6 @@ abstract class AbstractHelmMojo : AbstractMojo() {
 		}
 	}
 
-	protected fun majorHelmVersion(): Int = helmVersion.splitToSequence('.').first().toInt()
 
 	protected fun target() = File(project.build.directory).resolve("helm")
 
@@ -130,54 +67,6 @@ abstract class AbstractHelmMojo : AbstractMojo() {
 	protected fun chartFolder() = chartFolder ?: "src/main/helm/${chartName()}"
 
 	protected fun isChartFolderPresent() = File("${project.basedir}/${chartFolder()}").exists()
-
-	private fun downloadAndInstallHelm(artifact: Artifact, platformIdentifier: String) {
-
-		val fileName = "helm-v$helmVersion-$platformIdentifier"
-
-		val targetFile = artifact.file.toPath()
-		Files.createDirectories(targetFile.parent)
-
-		val url = helmDownloadUrl.resolve("./$fileName.zip").toURL()
-
-		downloadFileAndExtractBinary(url, targetFile)
-	}
-
-	private fun downloadFileAndExtractBinary(url: URL, destination: Path) {
-		val httpConnection = url.openConnection()
-		httpConnection.connect()
-		if (httpConnection !is HttpURLConnection || httpConnection.responseCode != 200) {
-			throw RuntimeException("Could not download file from $url")
-		}
-
-		val sizeInMiB: Double = httpConnection.contentLengthLong / 1024.0 / 1024.0
-		log.info("Downloading $url; need to get %.1f MiB...".format(sizeInMiB))
-
-		val downloadTimeMillis = measureTimeMillis {
-			httpConnection.inputStream.use {
-				ZipInputStream(it).use { zip ->
-					var entry: ZipEntry? = zip.nextEntry
-					do {
-						if (entry != null) {
-							if (isHelmBinary(entry)) {
-								Files.copy(zip, destination, StandardCopyOption.REPLACE_EXISTING)
-								zip.closeEntry()
-								break
-							} else {
-								zip.closeEntry()
-								entry = zip.nextEntry
-							}
-						}
-					} while (entry != null)
-				}
-			}
-		}
-
-		log.info("Download took %.1f seconds".format(downloadTimeMillis / 1000.0))
-	}
-
-	private fun isHelmBinary(entry: ZipEntry): Boolean =
-		!entry.isDirectory && (entry.name.endsWith("helm") || entry.name.endsWith("helm.exe"))
 
 	protected fun quoteFilePath(filePath: String): String =
 		if (filePath.contains(Regex("\\s"))) {
