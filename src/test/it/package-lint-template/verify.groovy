986b84b8e94f98137e34ea4617b27612d09f2dbf
Michael Walser <git@mwalser.net>
2023-03-24 10:15:09 +0100

    Apply suggestions from code review
    Co-authored-by: Henning Hoefer <hoeferh@gmail.com>

--- src/test/it/package-lint-template/verify.groovy
+++ src/test/it/package-lint-template/verify.groovy
@@ -1,11 +1,11 @@
 def logLines = new File(basedir, "build.log").readLines()
 
 // Package goal
-assert logLines.any { line -> line.contains("Successfully packaged chart and saved it to") }
+assert logLines.any { it.contains("Successfully packaged chart and saved it to") }
 
 // Lint goal
-assert logLines.any { line -> line.contains("Chart.yaml: icon is recommended") }
-assert logLines.any { line -> line.contains("1 chart(s) linted, 0 chart(s) failed") }
+assert logLines.any { it.contains("Chart.yaml: icon is recommended") }
+assert logLines.any { it.contains("1 chart(s) linted, 0 chart(s) failed") }
 
 // Template goal
 def templatedChart = new File(basedir, "target/test-classes/helm.yaml")
